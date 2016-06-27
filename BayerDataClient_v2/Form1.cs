7c68728f142db25a150b80c1b77efbddb73b35f3
Ryan <mosrya11@hotmail.com>
2016-06-27 16:26:32 +0100

    red columns on wrong chemical inclusions - v1

--- BayerDataClient_v2/Form1.cs
+++ BayerDataClient_v2/Form1.cs
@@ -249,6 +249,25 @@ namespace BayerDataClient_v4
 
         }
 
+        private void dataGridView1_DataBindingComplete(object sender,
+                       DataGridViewBindingCompleteEventArgs e)
+        {
+            if (e.ListChangedType != ListChangedType.ItemDeleted)
+            {
+                DataGridViewCellStyle red = dataGridView1.DefaultCellStyle.Clone();
+                red.BackColor = Color.Red;
+
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    if (r.Cells["FollowedUp"].Value.ToString()
+                           .ToUpper().Contains("NO"))
+                    {
+                        r.DefaultCellStyle = red;
+                    }
+                }
+            }
+        }
+
 
 
 
